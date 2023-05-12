afaf84011fcff7892f6ff5c54915a0e67224ecad
Dennis <denchikdron228@gmail.com>
2023-05-12 14:42:13 +0300

    Initial commit

---
+++ data.py
@@ -0,0 +1,17 @@
+question_data = [
+        {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+         "question": "Gumbo is a stew that originated in Louisiana.", "correct_answer": "True",
+         "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+                                           "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+                                           "correct_answer": "False", "incorrect_answers": ["True"]},
+        {"category": "Geography", "type": "boolean", "difficulty": "easy",
+         "question": "Ottawa is the capital of Canada.", "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"category": "Vehicles", "type": "boolean", "difficulty": "easy",
+         "question": "The full English name of the car manufacturer BMW is Bavarian Motor Works",
+         "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"category": "Animals", "type": "boolean", "difficulty": "easy",
+         "question": "A flock of crows is known as a homicide.", "correct_answer": "False",
+         "incorrect_answers": ["True"]}, {"category": "Politics", "type": "boolean", "difficulty": "easy",
+                                          "question": "One of Barack Obama&#039;s United States presidential campaign slogan&#039;s was &quot;Yes We Can&quot;.",
+                                          "correct_answer": "True", "incorrect_answers": ["False"]}
+]
